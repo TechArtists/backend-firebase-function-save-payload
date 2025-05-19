552b678a2f9e84732919ee60abefd9891694fc81
Andrei Damian <andrei.damian@joinappex.com>
2025-05-19 13:30:59 +0300

    fix comma lint issue

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -20,7 +20,7 @@ export const damaTest=onCall(
   {
     enforceAppCheck: true,
     secrets: [],
-    serviceAccount: "firebase-runtime@appex-data-imports.iam.gserviceaccount.com"
+    serviceAccount: "firebase-runtime@appex-data-imports.iam.gserviceaccount.com",
   },
   async (request) => {
     const data = request.data;
